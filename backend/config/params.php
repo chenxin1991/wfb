3c2da98a1e961a3144263b108122fb58a018dc0a
chenxin <2582526377@qq.com>
2019-05-08 17:36:37 +0800

    测试

--- backend/config/params.php
+++ backend/config/params.php
@@ -1,7 +1,5 @@
 <?php
 return [
-    // 图片服务器的域名设置，拼接保存在数据库中的相对地址，可通过web进行展示
-    'domain' => 'http://micropublish.6ekj.com/',
     'webuploader' => [
         // 后端处理图片的地址，value 是相对的地址
         'uploadUrl' => 'product/upload',

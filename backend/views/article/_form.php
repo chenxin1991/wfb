8f2a4bdee90928a3b45f1968b9b5db922cd418ad
chenxin <2582526377@qq.com>
2019-05-12 18:59:00 +0800

    统计字数wordcount

--- backend/views/article/_form.php
+++ backend/views/article/_form.php
@@ -93,7 +93,7 @@ if (!$model->isNewRecord){
             plugins:[
                 'advlist autolink lists link charmap print preview anchor',
                 'searchreplace visualblocks code fullscreen',
-                'insertdatetime media table contextmenu paste '
+                'insertdatetime media table contextmenu paste wordcount'
             ],
             toolbar:'undo redo | styleselect fontsizeselect| bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image fullscreen | keywordsButton websiteButton mainKeywordsButton longtailKeywordsButton',
             $setup

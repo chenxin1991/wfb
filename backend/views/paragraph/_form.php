f8e775498570a0f9d1b2887fa80365cfa14cab90
chenxin <2582526377@qq.com>
2019-05-21 09:18:07 +0800

    修改bug

--- backend/views/paragraph/_form.php
+++ backend/views/paragraph/_form.php
@@ -3,10 +3,17 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use backend\models\Product;
+use dosamigos\tinymce\TinyMce;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\Paragraph */
 /* @var $form yii\widgets\ActiveForm */
+?>
+
+<?php
+$url=Yii::$app->assetManager->getPublishedUrl('@vendor/2amigos/yii2-tinymce-widget/src/assets');
+$domain=Yii::$app->params['domain'];
+$setup="";
 ?>
 
 <div class="box-body">
@@ -21,15 +28,24 @@ use backend\models\Product;
     ['prompt'=>'', 'data-placeholder'=>'类型', 'class'=>'form-control select2', 'style'=>'width:100%']) ?>
     <?=$form->field($model, 'product_id')->dropDownList(Product::all(), 
     ['prompt' => '', 'data-placeholder' => '选择产品', 'class' => 'form-control select2', 'style' => 'width:100%;'])?>
-    <?= $form->field($model, 'content')->widget(\yii\redactor\widgets\Redactor::className(), [
-        'clientOptions' => [
-            'imageManagerJson' => ['/redactor/upload/image-json'],
-            'imageUpload' => ['/redactor/upload/image'],
-            'fileUpload' => ['/redactor/upload/file'],
-            'lang' => 'zh_cn',
-            'plugins' => ['clips', 'fontcolor','imagemanager']
-        ]
-    ])?>
+    <?= $form->field($model, 'content')->widget(TinyMce::className(), [
+        'language' => 'zh_CN',
+        'clientOptions' => "{
+            selector: '#paragraph-content',
+            height: 600,
+            language_url:'{$url}',
+            language:'zh_CN',
+            relative_urls:false,
+            remove_script_host:false,
+            document_base_url:'{$domain}',
+            plugins:[
+                'advlist autolink lists link charmap print preview anchor',
+                'searchreplace visualblocks code fullscreen',
+                'insertdatetime media table contextmenu paste wordcount'
+            ],
+            toolbar:'undo redo | styleselect fontsizeselect| bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image fullscreen | keywordsButton websiteButton mainKeywordsButton longtailKeywordsButton',
+        }"
+    ]);?>
     <div class="box-footer">
         <div class="col-sm-4"></div>
         <div class="col-sm-2 col-xs-2"><a href="javascript:history.back();" class="btn btn-default">取消</a></div>
